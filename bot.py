38c59be6dd7a3473c77a421601d46c8e09405b0b
d-t-n <danilo.trancoso.neves@gmail.com>
2023-10-28 20:04:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ def load_llm():
 #QA Model Function
 def qa_bot():
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                       model_kwargs={'device': 'cpu'})
+                                       model_kwargs={'device': 'mps'})
     db = FAISS.load_local(DB_FAISS_PATH, embeddings)
     llm = load_llm()
     qa_prompt = set_custom_prompt()
